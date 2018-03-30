1ac6a27d7f8dc1f354ca3f430e120b1f70dba874
root <root@localhost.localdomain>
2018-03-30 06:14:20 -0500

    Adding wordlist script

---
+++ hashTools/getWordListsFromDropBox.sh
@@ -0,0 +1,6 @@
+wget https://www.dropbox.com/s/m47k1cunttqnoak/small-dict-cracklib-wd.txt
+wget https://www.dropbox.com/s/joa46q1718ugbpv/rockyou-cp.txt
+wget https://www.dropbox.com/s/ooagz8wm4nwv8ue/markburnett-10m-cp.txt
+wget https://www.dropbox.com/s/u92cbaskcw6npjr/large-dict-wd.txt
+wget https://www.dropbox.com/s/qjcerxv56j0rxa4/firstnames-1990census-pn.txt
+
